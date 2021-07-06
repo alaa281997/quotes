22a0f5448228fd26c73faf6535117ec18c458a61
alaa281997 <alaabbas97@gmail.com>
2021-07-07 01:05:46 +0300

    Add lab 8

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Lab 8 OO Design and GSON
+
+> In this Application we need to use json file depend on our object, so we need to use GSON: 
+> Gson is a Java library that can be used to convert Java Objects into their JSON representation.
+> The app should showing the quote and the author when it is run and choose one quote each time it is run.
---
+++ app/src/main/java/quotes/quote.java
@@ -0,0 +1,57 @@
+package quotes;
+
+import java.util.Arrays;
+
+public class quote {
+    private String[] tags;
+    private String author;
+    private String likes;
+    private  String text;
+
+    public quote(String[] tags, String author, String likes, String text) {
+        this.tags = tags;
+        this.author = author;
+        this.likes = likes;
+        this.text = text;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "quote{" +
+                " Author :'" + author + '\'' +
+                ", text :'" + text + '\'' +
+                '}';
+    }
+}
